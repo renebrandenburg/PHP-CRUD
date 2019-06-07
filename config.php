89449206c6e4abdcfc705dd7a88dd1d3b558517f
Rene Brandenburg <rbrandenburg@computest.nl>
2019-06-07 13:07:33 +0200

    update config.php

--- config.php
+++ config.php
@@ -1,15 +1,13 @@
 <?php
-
 /**
-  * Configuration for database connection
-  *
-  */
-
+ * Configuration for database connection
+ *
+ */
 $host       = "localhost";
 $username   = "root";
 $password   = "root";
-$dbname     = "test"; // will use later
-$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$dbname     = "test";
+$dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
